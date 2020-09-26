a249937694120c2cddb4801fdb42f3b6058efd66
Md Nahid <54438631+whaiteking@users.noreply.github.com>
2020-09-26 05:59:16 -0700

    1st_psd_to_html
    this is main file

---
+++ js/my.js
@@ -0,0 +1,122 @@
+$('svg.radial-progress').each(function( index, value ) { 
+    $(this).find($('circle.complete')).removeAttr( 'style' );
+  });
+
+
+  $(window).scroll(function(){
+    $('svg.radial-progress').each(function( index, value ) { 
+      // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+      if ( 
+          $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+          $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+      ) {
+          // Get percentage of progress
+          percent = $(value).data('percentage');
+          // Get radius of the svg's circle.complete
+          radius = $(this).find($('circle.complete')).attr('r');
+          // Get circumference (2πr)
+          circumference = 2 * Math.PI * radius;
+          // Get stroke-dashoffset value based on the percentage of the circumference
+          strokeDashOffset = circumference - ((percent * circumference) / 100);
+          // Transition progress for 1.25 seconds
+          $(this).find($('circle.complete')).animate({'stroke-dashoffset': strokeDashOffset}, 1250);
+      }
+    });
+  }).trigger('scroll');
+
+
+// here is my filter menu customaice
+
+
+  $(document).ready(function(){
+      var mixer = mixitup('.flt');
+  });
+
+
+  // key
+
+  var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
